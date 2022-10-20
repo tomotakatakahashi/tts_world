5f74eaa1e9036b6661ac3b70bfbac6766f157444
Tomotaka Takahashi <20836307+tomotakatakahashi@users.noreply.github.com>
2022-10-20 21:54:18 +0900

    Format

--- tts_world/train_duration.py
+++ tts_world/train_duration.py
@@ -12,31 +12,51 @@ LINGUISTIC_DIR = GENERATED_DIR / "linguistic"
 
 DATA_SIZE = 4000
 
-duration_paths = sorted(DURATION_DIR.glob("*"))[:DATA_SIZE] # Remove mean.npy and std.npy
+duration_paths = sorted(DURATION_DIR.glob("*"))[
+    :DATA_SIZE
+]  # Remove mean.npy and std.npy
 linguistic_paths = sorted(LINGUISTIC_DIR.glob("*"))[:DATA_SIZE]
 
 duration_arrays = [np.load(path) for path in duration_paths]
 linguistic_arrays = [np.load(path) for path in linguistic_paths]
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64,),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Dense(64,),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Dense(64,),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Dense(1, activation=None),
-])
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Dense(
+            64,
+        ),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dense(
+            64,
+        ),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dense(
+            64,
+        ),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dense(1, activation=None),
+    ]
+)
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
-              loss=tf.keras.losses.MeanSquaredError(),
-              metrics=['MAE'])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+    loss=tf.keras.losses.MeanSquaredError(),
+    metrics=["MAE"],
+)
 
-BATCH_SIZE = 2 ** 13
+BATCH_SIZE = 2**13
 
-dataset = tf.data.Dataset.from_tensor_slices(
-    (np.concatenate(linguistic_arrays, axis=0), np.concatenate(duration_arrays, axis=0))
-).shuffle(BATCH_SIZE * 10).batch(BATCH_SIZE).prefetch(10)
+dataset = (
+    tf.data.Dataset.from_tensor_slices(
+        (
+            np.concatenate(linguistic_arrays, axis=0),
+            np.concatenate(duration_arrays, axis=0),
+        )
+    )
+    .shuffle(BATCH_SIZE * 10)
+    .batch(BATCH_SIZE)
+    .prefetch(10)
+)
 model.fit(dataset, epochs=100)
 
 model.save("duration_model.h5")
